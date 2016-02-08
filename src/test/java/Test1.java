4333fa81a52ad121d30b18a0c3e34d2d18f435b7
Roi <roioteromorales@gmail.com>
2016-02-08 19:37:36 +0100

    2 Commit

---
+++ src/test/java/Test1.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * Created by Roi on 08-Feb-16.
+ */
+public class Test1 {
+
+
+    @Test
+    public void testName() throws Exception {
+        System.out.println("********************************************");
+        System.out.println("Building tests");
+        System.out.println("********************************************");
+    }
+}
