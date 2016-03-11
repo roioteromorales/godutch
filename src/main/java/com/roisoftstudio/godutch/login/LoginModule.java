d3fc7496ee6a303e0863a90011ac3d5476e69cac
Roi <roioteromorales@gmail.com>
2016-03-11 19:57:03 +0100

    Try to put guice.... fail

--- src/main/java/com/roisoftstudio/godutch/login/LoginModule.java
+++ src/main/java/com/roisoftstudio/godutch/login/LoginModule.java
@@ -6,7 +6,7 @@ import com.roisoftstudio.godutch.login.db.dao.UserDao;
 import com.roisoftstudio.godutch.login.services.DefaultSignService;
 import com.roisoftstudio.godutch.login.services.SignService;
 
-public class LoginModule extends AbstractModule {
+public class LoginModule extends AbstractModule {//ServletModule {
 
     @Override
     protected void configure() {
