4130503341f4a461e9af095d91fdd7e92ba13607
Roi <roioteromorales@gmail.com>
2016-04-02 17:06:17 +0200

    Write first functional end to end test

--- src/test/java/com/roisoftstudio/godutch/integration/login/paths/SignPathTest.java
+++ src/test/java/com/roisoftstudio/godutch/integration/login/paths/SignPathTest.java
@@ -1,30 +1,18 @@
 package com.roisoftstudio.godutch.integration.login.paths;
 
-import com.sun.jersey.test.framework.AppDescriptor;
-import com.sun.jersey.test.framework.JerseyTest;
-import com.sun.jersey.test.framework.WebAppDescriptor;
+import com.github.kevinsawicki.http.HttpRequest;
+import org.junit.Test;
 
-public class SignPathTest extends JerseyTest {
+import static org.assertj.core.api.Assertions.assertThat;
 
-    @Override
-    protected AppDescriptor configure() {
-        return new WebAppDescriptor.Builder().build();
+public class SignPathTest {
+
+    @Test
+    public void simpleIntegrationTest() throws Exception {
+        String response = HttpRequest.get("http://192.168.99.100:18080/godutch-0.1/sign/help").body();
+        System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+        System.out.println("@@           INTEGRATION TEST IS RUNNING          @@");
+        System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+        assertThat(response).isEqualTo("This is working");
     }
-
-    //todo fix tests with good ip and parameters
-//    @Test
-//    public void userCanSignUp() throws JSONException, URISyntaxException {
-//        WebResource webResource = client().resource("http://192.168.99.100:18080/");
-//        JSONObject json = webResource.path("godutch-0.1/sign/up").put(JSONObject.class);
-//        assertThat(json.get("id"), is("12"));
-//        assertThat(json.get("firstName"), is("Tim"));
-//        assertThat(json.get("lastName"), is("Tester"));
-//        assertThat(json.get("birthday"), is("1970-01-16T07:56:49.871+01:00"));
-//    }
-//
-//    @Test(expected = UniformInterfaceException.class)
-//    public void testUserNotFound() {
-//        WebResource webResource = client().resource("http://localhost:8080/");
-//        JSONObject json = webResource.path("/rest-test-tutorial/user/id/666").get(JSONObject.class);
-//    }
 }
