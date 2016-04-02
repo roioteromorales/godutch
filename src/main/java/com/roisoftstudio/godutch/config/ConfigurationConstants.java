f3719528be40dff975ae6540b25b8e3c7f058288
Roi <roioteromorales@gmail.com>
2016-04-02 17:51:16 +0200

    fix functional test to execute on remote

--- src/main/java/com/roisoftstudio/godutch/config/ConfigurationConstants.java
+++ src/main/java/com/roisoftstudio/godutch/config/ConfigurationConstants.java
@@ -2,5 +2,12 @@ package com.roisoftstudio.godutch.config;
 
 public class ConfigurationConstants {
     public static final String CONFIG_FILE_NAME = "config.properties";
+    public static PropertiesReader propertiesReader = new PropertiesReader(CONFIG_FILE_NAME);
+    public static final String PORT = propertiesReader.getValue("port");
+
+    public static final String DOCKER_HOST = System.getenv("DOCKER_HOST");
+    public static final String CONTAINER_URL = DOCKER_HOST
+            .replace("tcp", "http")
+            .subSequence(0, DOCKER_HOST.length() - 3) + PORT + "/godutch-" +  "0.1" + "/"; // TODO SOMETHING WITH VERSION AND PATH
 
 }
--- src/test/java/com/roisoftstudio/godutch/integration/login/paths/SignPathTest.java
+++ src/test/java/com/roisoftstudio/godutch/integration/login/paths/SignPathTest.java
@@ -3,16 +3,20 @@ package com.roisoftstudio.godutch.integration.login.paths;
 import com.github.kevinsawicki.http.HttpRequest;
 import org.junit.Test;
 
+import static com.roisoftstudio.godutch.config.ConfigurationConstants.CONTAINER_URL;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SignPathTest {
 
+
     @Test
     public void simpleIntegrationTest() throws Exception {
-        String response = HttpRequest.get("http://192.168.99.100:18080/godutch-0.1/sign/help").body();
         System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
         System.out.println("@@           INTEGRATION TEST IS RUNNING          @@");
         System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+        System.out.println("URL: " + CONTAINER_URL);
+
+        String response = HttpRequest.get(CONTAINER_URL + "sign/help").body();
         assertThat(response).isEqualTo("This is working");
     }
 }
