a4566ff4e64f82ef4351d3cd76d7f1bf5c4a0404
Roi <roioteromorales@gmail.com>
2016-04-12 22:38:55 +0200

    Change import logger

--- src/main/java/com/roisoftstudio/godutch/info/services/InfoService.java
+++ src/main/java/com/roisoftstudio/godutch/info/services/InfoService.java
@@ -3,6 +3,7 @@ package com.roisoftstudio.godutch.info.services;
 import com.google.gson.Gson;
 import com.roisoftstudio.godutch.config.PropertiesReader;
 import com.roisoftstudio.godutch.info.model.InfoModel;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.LocalDateTime;
@@ -10,7 +11,7 @@ import java.time.LocalDateTime;
 import static com.roisoftstudio.godutch.config.ConfigurationConstants.CONFIG_FILE_NAME;
 
 public class InfoService {
-    final org.slf4j.Logger logger = LoggerFactory.getLogger(InfoService.class);
+    final Logger logger = LoggerFactory.getLogger(InfoService.class);
 
     private final InfoModel infoModel;
     private final PropertiesReader propertiesReader;
